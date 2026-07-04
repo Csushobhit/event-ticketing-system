2a8fc9e20e316e380aa8b4eb0cb458c35c30e2e6
Csushobhit <csushobhit5515@gmail.com>
2026-07-05 01:18:00 +0530

    Add JPA entities, relationships, repositories and Spring Security authentication foundation

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4d/20562a74df7700111bfc8447cf285282
@@ -0,0 +1,9 @@
+package com.event.ticketing.eventticketingsystem.repository;
+
+import com.event.ticketing.eventticketingsystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
--- event-ticketing-system/src/main/java/com/event/ticketing/eventticketingsystem/model/User.java
+++ event-ticketing-system/src/main/java/com/event/ticketing/eventticketingsystem/model/User.java
@@ -5,10 +5,17 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Builder
@@ -16,7 +23,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(
@@ -61,4 +68,36 @@ public class User {
         orphanRemoval = true
     )
     private List<Order> orders = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
