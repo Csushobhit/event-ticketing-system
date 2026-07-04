170f5466e7cbdfdcb7b6d64d213fd245695e3a57
Csushobhit <csushobhit5515@gmail.com>
2026-07-04 23:27:47 +0530

    Implement User-Event JPA relation

---
+++ event-ticketing-system/src/main/java/com/event/ticketing/eventticketingsystem/model/Role.java
@@ -0,0 +1,7 @@
+package com.event.ticketing.eventticketingsystem.model;
+
+public enum Role {
+	ROLE_USER,
+	ROLE_ORGANIZER,
+	ROLE_ADMIN
+}
---
+++ event-ticketing-system/src/test/java/com/event/ticketing/event_ticketing_system/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.ticketing.event_ticketing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
